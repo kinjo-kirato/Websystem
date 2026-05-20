509e346a18d87b615c124671f650225f5bd9684f
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-20 15:43:20 +0900

    DbContext

---
+++ WebEmployeeManagement/Infrastructures/Entities/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebEmployeeManagement.Infrastructures.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+    }
+}
